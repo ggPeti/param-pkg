695441c95b9a2518bbaf5a9c2428f49811489b25
Peter Ferenczy <ggpeti@gmail.com>
2024-01-17 21:02:10 +0100

    release

---
+++ README.md
@@ -0,0 +1,15 @@
+Poor man's configurable derivation builder
+
+Usage: 
+```
+nix run github:ggpeti/param-pkg myflake#lib.mkMyPkg '<arg-expr>'
+```
+
+Takes a textual nix expression as the final argument. `myflake#lib.mkMyPkg` is a function of type any -> derivation.
+
+Uses `nix eval` under the hood but produces the usual `result` symlink in the cwd.
+
+Hacks utilized:
+1. flakes don't support [configurable](https://github.com/NixOS/nix/issues/2861) [outputs](https://github.com/NixOS/nix/pull/6583) and `--argstr` -> but there is `nix eval --apply`
+2. `nix eval` doesn't instantiate derivation outputs until they are accessed -> `builtins.readFileType` does that
+3. lazy evaluation prevents instantiation as well -> but as is widely known, nix is eager in attrset names.
--- flake.nix
+++ flake.nix
@@ -1,21 +1,27 @@
 {
-  description = "A very basic flake";
+  description = "Parametrize packages";
 
-  outputs = { self, nixpkgs }:
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
     in
     {
-      hello = a: pkgs.writeShellScriptBin "hello" ''
+      lib.hello = a: pkgs.writeShellScriptBin "hello" ''
         echo "Hello, ${a}!"
       '';
 
-      apps.${system}.parametrize = {
-        type = "app";
-        program = "${pkgs.writeShellScriptBin "parametrize" ''
+    } //
+    (flake-utils.lib.eachDefaultSystem (system: {
+      apps.parametrize = flake-utils.lib.mkApp {
+        drv = pkgs.writeShellScriptBin "parametrize" ''
+          if 
+          echo "Usage: parametrize flake#lib.mkPkg '<nix-expr>'"
           ln -sfn "$(nix eval "$1" --raw --apply 'f: "''${rec { pkg = f '"$2"'; ''${builtins.readFileType "''${pkg}"} = 1;}.pkg}"')" result
-        ''}/bin/parametrize";
+        '';
       };
-    };
+    }));
 }
+    
